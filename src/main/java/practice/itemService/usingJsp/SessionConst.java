1c27145065d77872089260889ad887bddb613285
OhJeongGil <jeongkeepscalm@gmail.com>
2024-02-18 21:37:15 +0900

    test

--- src/main/java/practice/itemService/usingJsp/SessionConst.java
+++ src/main/java/practice/itemService/usingJsp/SessionConst.java
@@ -1,7 +1,7 @@
 package practice.itemService.usingJsp;
 
 public class SessionConst {
-
+    // test
     public static final String LOGIN_USER = "loginUser";
     public static final int LOGIN_SESSION_TIME = 60 * 60;
 
